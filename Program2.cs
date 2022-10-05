b6e996d07b1b1bad58bfa13ff539bd7bae1518ac
Foxy4u <79055645+Foxy4u@users.noreply.github.com>
2022-10-05 11:40:22 +0200

    o
    k zadania 1-6 2 karta pracy

---
+++ Program2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Zad 1
+            /*
+             int m = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine( i * i * i + 3);
+            }
+            */
+
+            //Zad 2
+            /*forint=105; i < 1000; i = i + 15)
+             {
+                 Console.WriteLine(i);
+             }
+             */
+
+            //Zad 3
+            /*
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < -n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            */
+
+            //Zad 4
+            /*
+            int suma = 0;
+            for (int i = 10; i < 100; i++)
+            {
+                suma = suma + i;
+            }
+            Console.WriteLine(suma);   
+            */
+
+            //Zad 5
+            /*
+            Console.WriteLine("Podaj liczbę w którą gramy (n): ");
+            int n = int.Parse(Console.ReadLine());
+            int suma = (n * (n + 1) / 2;
+            Console.WriteLine(suma);
+            */
+
+            //Zad 6
+
+        }
+    }
+}
